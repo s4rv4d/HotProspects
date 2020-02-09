1b750c4545cdf673fe98c06007b411f8cb01ccb9
s4rv4d <sarvadshetty@gmail.com>
2020-02-09 09:00:12 +0530

    cleaner unblocking api temp for network calls using Result type

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -13,24 +13,51 @@ struct ContentView: View {
     @State private var selectedTab = 0
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab1")
-                .onTapGesture {
-                    self.selectedTab = 1
-            }
-                .tabItem {
-                    Image(systemName: "star")
-                    Text("One")
-            }
-            .tag(0)
-            Text("Tab2")
-            .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Two")
-            }
-        .tag(1)
+        Text("Hello, World!")
+            .onAppear {
+                self.fetchData(from: "https://www.apple.com") { (result) in
+                    switch result {
+                    case .success(let str) :
+                        print(str)
+                    case .failure(let error) :
+                        switch error {
+                        case .badURL :
+                            print("Bad url")
+                        case .requestFailed :
+                            print("Bad url")
+                        case .unknown :
+                            print("Unknown error")
+                        }
+                    }
+                }
         }
     }
+    
+    func fetchData(from urlString: String, completion: @escaping(Result<String, NetworkError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.badURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            //the task has completed push work to main thread
+            DispatchQueue.main.async {
+                if let data = data {
+                    //deocde to string
+                    let stringData = String(decoding: data, as: UTF8.self)
+                    completion(.success(stringData))
+                } else if error != nil {
+                    completion(.failure(.requestFailed))
+                } else {
+                    completion(.failure(.unknown))
+                }
+            }
+        }.resume()
+    }
+}
+
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
 }
 
 struct ContentView_Previews: PreviewProvider {
